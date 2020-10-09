2e0eb61dbebffc0bfa4697cc7f03ce571beece08
Zach <zachzeisler1@gmail.com>
2020-10-09 12:53:34 -0400

    Organized MFC/HC/amygdala data in similar format to Steinmetz

---
+++ Human MFC/build_structures.m
@@ -0,0 +1,81 @@
+%% Load in data
+
+clear
+
+load('D:\Human medial frontal cortex\spikes\ha.mat')
+load('D:\Human medial frontal cortex\spikes\mfc.mat')
+
+%% Loop through rows in mfc and ha structures to pull out timeseries for single cells
+
+% MFC
+
+mfc_units = cell(1,length(data_mfc));
+
+for row =1:length(data_mfc)
+   
+    session = data_mfc(row).sessionID;
+    response_ts = data_mfc(row).ts.reply;
+    cluster = data_mfc(row).cellinfo(2);
+    brain_area = data_mfc(row).cellinfo(3);
+    
+    if brain_area == 1 || brain_area == 5
+        brain_area = 'amygdala';
+    elseif brain_area == 2 || brain_area == 6
+        brain_area = 'dorsal ACC';
+    elseif brain_area == 3 || brain_area == 7
+        brain_area = 'hippocampus';
+    elseif brain_area == 4 || brain_area == 8
+        brain_area = 'preSMA';
+    end
+    
+    row_struct = struct('SessionID',session,'ClusterNumber',cluster,'BrainArea',brain_area,'SpikeTimes',response_ts);
+    
+    mfc_units{row} = row_struct;
+    
+end
+
+% Hippocampus and amygdala
+
+ha_units = cell(1,length(data_ha));
+
+for row =1:length(data_ha)
+   
+    session = data_ha(row).sessionID;
+    response_ts = data_ha(row).ts.reply;
+    cluster = data_ha(row).cellinfo(2);
+    brain_area = data_ha(row).cellinfo(3);
+    
+    spikes = cell(1,1);
+    spikes{1,1} = response_ts;
+    
+    if brain_area == 1 || brain_area == 5 % smaller value is left, larger is right
+        brain_area = 'amygdala';
+    elseif brain_area == 2 || brain_area == 6
+        brain_area = 'dorsal ACC';
+    elseif brain_area == 3 || brain_area == 7
+        brain_area = 'hippocampus';
+    elseif brain_area == 4 || brain_area == 8
+        brain_area = 'preSMA';
+    end
+    
+    row_struct = struct('SessionID',session,'ClusterNumber',cluster,'BrainArea',brain_area,'SpikeTimes',spikes);
+    
+    ha_units{row} = row_struct;
+    
+end
+
+save('human_MFC.mat','mfc_units','ha_units')
+
+%% What does the structure look like?
+%{
+    Each entry is a session
+    Each session contains the following information:
+
+        Session ID - to map back to original dataset
+        ClusterNumber - in case two channels had spikes from the same cell
+        BrainArea - which brain region was this from?
+        SpikeTimes - response-aligned spike times, each cell is a trial;
+        b/c these are aligned to responses, positive values occur during
+        the 1-2sec ITI
+
+%}
