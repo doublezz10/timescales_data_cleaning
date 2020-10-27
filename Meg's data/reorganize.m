76d03ee155f366ef9c33ba7d5e4f4f7df38ce9f0
Zach Zeisler <zachzeisler1@gmail.com>
2020-10-27 17:52:44 -0400

    Finally actually done

--- Meg's data/reorganize.m
+++ Meg's data/reorganize.m
@@ -1,3 +1,5 @@
+clear
+
 % load('D:\Data from Meg\units_for_Zach.mat')
 load('/Users/zachz/Downloads/units_for_Zach.mat')
 
@@ -26,12 +28,12 @@ for unit=1:length(units)
     ap_coord = units(unit).AP;
     total_depth = units(unit).total_depth;
     
+    fixation_spikes = [];
+    trial_spikes = [];
+    
     if strcmp(brain_area,'SC') == 1
         brain_area = 'scACC';
         cell_info_scACC{end+1} = struct('Dataset',dataset,'Species',species,'Unit_name',unit_name,'Brain_area',brain_area,'AP',ap_coord,'ML',ml_coord,'total_depth',total_depth);
-        
-        fixation_spikes = [];
-        trial_spikes = [];
         
         for trial = 1:length(units(unit).fixOn)
         
@@ -42,12 +44,12 @@ for unit=1:length(units)
             spikes = units(unit).spikes;
             
             for spike = 1:length(spikes)
-                if spike >= fixation_start && spike <= fixation_end
-                    fixation_spikes(end+1) = spike;
-                end
+                if spikes(spike) >= fixation_start && spikes(spike) <= fixation_end
+                    fixation_spikes(end+1) = spikes(spike);
                 
-                if spike >= task_start && spike <= task_end
-                    trial_spikes(end+1) = spike;
+                elseif spikes(spike) >= task_start && spikes(spike) <= task_end
+                    trial_spikes(end+1) = spikes(spike);
+                    
                 end
                 
             end
@@ -58,10 +60,7 @@ for unit=1:length(units)
     rest_scACC{end+1} = fixation_spikes;
     task_scACC{end+1} = trial_spikes;
         
-    
-    end
-    
-    if strcmp(brain_area,'AMY') == 1
+    elseif strcmp(brain_area,'AMY') == 1
         brain_area = 'amygdala';
         cell_info_amygdala{end+1} = struct('Dataset',dataset,'Species',species,'Unit_name',unit_name,'Brain_area',brain_area,'AP',ap_coord,'ML',ml_coord,'total_depth',total_depth);
         
@@ -77,12 +76,11 @@ for unit=1:length(units)
             spikes = units(unit).spikes;
             
             for spike = 1:length(spikes)
-                if spike >= fixation_start && spike <= fixation_end
-                    fixation_spikes(end+1) = spike;
-                end
-                
-                if spike >= task_start && spike <= task_end
-                    trial_spikes(end+1) = spike;
+                if spikes(spike) >= fixation_start && spikes(spike) <= fixation_end
+                    fixation_spikes(end+1) = spikes(spike);
+
+                elseif spikes(spike) >= task_start && spikes(spike) <= task_end
+                    trial_spikes(end+1) = spikes(spike);
                 end
                 
             end
@@ -91,12 +89,8 @@ for unit=1:length(units)
         
     rest_amygdala{end+1} = fixation_spikes;
     task_amygdala{end+1} = trial_spikes;
-            
     
-    end
-    
-    
-    if strcmp(brain_area,'VS') == 1
+    elseif strcmp(brain_area,'VS') == 1
         brain_area = 'ventralStriatum';
         cell_info_ventralStriatum{end+1} = struct('Dataset',dataset,'Species',species,'Unit_name',unit_name,'Brain_area',brain_area,'AP',ap_coord,'ML',ml_coord,'total_depth',total_depth);
         
@@ -112,12 +106,11 @@ for unit=1:length(units)
             spikes = units(unit).spikes;
             
             for spike = 1:length(spikes)
-                if spike >= fixation_start && spike <= fixation_end
-                    fixation_spikes(end+1) = spike;
-                end
-                
-                if spike >= task_start && spike <= task_end
-                    trial_spikes(end+1) = spike;
+                if spikes(spike) >= fixation_start && spikes(spike) <= fixation_end
+                    fixation_spikes(end+1) = spikes(spike);
+
+                elseif spikes(spike) >= task_start && spikes(spike) <= task_end
+                    trial_spikes(end+1) = spikes(spike);
                 end
                 
             end
