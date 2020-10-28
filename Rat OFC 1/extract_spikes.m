b8162514d6dcdaec7c89f809f9f6f0f03fdc3ec2
Zach <zachzeisler1@gmail.com>
2020-10-28 17:30:59 -0400

    Initial commit on rat ofc dataset 1

---
+++ Rat OFC 1/extract_spikes.m
@@ -0,0 +1,34 @@
+clear
+
+filenames = getfn('D:\CRCNS OFC 1 - Rats\data');
+
+timeseries = {};
+num_trials = [];
+trial_start = {};
+odor_valve_on = {};
+water_valve_off = {};
+
+for file = 1:length(filenames)
+   
+    this_file = filenames{file};
+    
+    load(this_file);
+    
+    if exist('TS','var')
+        
+        timeseries{end+1} = TS;
+        
+    elseif exist('TrialStart','var')
+        
+        num_trials(end+1) = length(TrialStart);
+        trial_start{end+1} = TrialStart;
+        odor_valve_on{end+1} = OdorValveOn;
+        water_valve_off{end+1} = WaterValveOff;
+        
+    end 
+    
+    clearvars -except filenames timeseries num_trials trial_start odor_valve_on water_valve_off
+    
+end
+
+save('rat_ofc_1.mat','num_trials','odor_valve_on','timeseries','trial_start','water_valve_off')
