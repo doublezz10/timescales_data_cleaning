f5085f876e112c6dcdd23b59a80b393d0a56b415
Zach Zeisler <zachzeisler1@gmail.com>
2020-11-10 17:02:15 -0500

    mac commit - MFC should be done here maybe

--- Human MFC/trial_spikes_by_trial.py
+++ Human MFC/trial_spikes_by_trial.py
@@ -12,39 +12,35 @@ import scipy.io as spio
 
 #%% Load in .mat
 
-trial_spikes = spio.loadmat('/Users/zachz/Downloads/trial_spikes.mat',simplify_cells=True)
+trial_spikes = spio.loadmat('/Users/zachz/Downloads/trial_spikes_no_realign.mat',simplify_cells=True)
 
 ha_trial_spikes = trial_spikes['all_ha_units_trialspikes']
 mfc_trial_spikes = trial_spikes['all_mfc_units_trialspikes']
 
 #%% hstack
 
-ha_hstacked = []
+ha_by_trial = []
 
 for unit in range(len(ha_trial_spikes)):
     
     unit_by_trial = ha_trial_spikes[unit]
     
-    unit_by_trial_hstack = np.hstack(unit_by_trial)
+    ha_by_trial.append(unit_by_trial)
     
-    ha_hstacked.append(unit_by_trial_hstack)
-    
-mfc_hstacked = []
+mfc_by_trial = []
 
 for unit in range(len(mfc_trial_spikes)):
     
     unit_by_trial = mfc_trial_spikes[unit]
     
-    unit_by_trial_hstack = np.hstack(unit_by_trial)
-    
-    mfc_hstacked.append(unit_by_trial_hstack)
+    mfc_by_trial.append(unit_by_trial)
     
 #%% Make dictionaries
 
-ha_dict = {'ha_spiketimes': ha_hstacked}
-mfc_dict = {'mfc_spiketimes': mfc_hstacked}
+ha_dict = {'ha_spiketimes': ha_by_trial}
+mfc_dict = {'mfc_spiketimes': mfc_by_trial}
     
 #%% Resave as mat
 
-spio.savemat('ha_spiketimes.mat',ha_dict)
-spio.savemat('mfc_spiketimes.mat',mfc_dict)
+spio.savemat('ha_spiketimes_by_trial.mat',ha_dict)
+spio.savemat('mfc_spiketimes_by_trial.mat',mfc_dict)
